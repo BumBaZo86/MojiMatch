a765b0710788caf7914bcc70550d16afc1f089d0
camillafalk <ccamilla.falk@gmail.com>
2025-05-22 11:12:57 +0200

    Play again button works so player plays again with the exact same arguments as the game before. Added a homebutton in GameOverView, it closes the game and you end up in HomeView with tabview available.

--- MojiMatch/MojiMatch/MojiMatch/Views/GameView.swift
+++ MojiMatch/MojiMatch/MojiMatch/Views/GameView.swift
@@ -15,6 +15,7 @@ struct GameView: View {
     @Binding var category: String
     @Binding var time: Double
     @Binding var noOfQuestions: Int
+    @Binding var showGameView: Bool
     
     @State var questionCount = 0
     @State var score = 0
@@ -85,7 +86,7 @@ struct GameView: View {
                     
                     Spacer(minLength: 90)
                     
-                    NavigationLink(destination: GameOverView(score: score), isActive: $isGameOver) {
+                    NavigationLink(destination: GameOverView(score: score, showGameView: $showGameView, category: $category, time: $time, noOfQuestions: $noOfQuestions), isActive: $isGameOver) {
                         EmptyView()
                     }
                 }

2908d45fc50cbb41316b04ced44525e0746d80d1
nasa01-crypto <natalie.samaan@live.se>
2025-05-30 19:19:14 +0200

    now saving on user profile when logout or closing the app and start again

--- MojiMatch/MojiMatch/MojiMatch/ViewModels/AppSettings.swift
+++ MojiMatch/MojiMatch/MojiMatch/ViewModels/AppSettings.swift
@@ -4,9 +4,52 @@
 //
 //  Created by Natalie S on 2025-05-30.
 //
-import SwiftUI
+import Foundation
+import Firebase
+import FirebaseFirestore
 import Combine
+import FirebaseAuth
+
 
 class AppSettings: ObservableObject {
-    @Published var isSettingsMode: Bool = false
+    @Published var isSettingsMode: Bool = false {
+        didSet {
+            saveDarkModeSetting()
+        }
+    }
+
+    private var db = Firestore.firestore()
+    private var userEmail: String? {
+        Auth.auth().currentUser?.email
+    }
+
+    init() {
+        loadDarkModeSetting()
+    }
+
+    func loadDarkModeSetting() {
+        guard let email = userEmail else { return }
+
+        db.collection("users").document(email).getDocument { document, error in
+            if let document = document, document.exists {
+                if let darkMode = document.data()?["darkMode"] as? Bool {
+                    DispatchQueue.main.async {
+                        self.isSettingsMode = darkMode
+                    }
+                }
+            }
+        }
+    }
+
+    func saveDarkModeSetting() {
+        guard let email = userEmail else { return }
+
+        db.collection("users").document(email).updateData([
+            "darkMode": isSettingsMode
+        ]) { error in
+            if let error = error {
+                print("Failed to save dark mode setting: \(error.localizedDescription)")
+            }
+        }
+    }
 }
